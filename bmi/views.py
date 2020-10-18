a7a36a7bf6c58e3f083b8397d3f84bc1d2dd9b76
ghosthapper <shubhankansu11@gmail.com>
2020-10-18 18:47:59 +0530

    bmi demo is done

--- bmi/views.py
+++ bmi/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def calculateBMI(weight, height):
+    return weight / (height ** 2)
+
+def index(request):
+    context = {'bmi': calculateBMI(65,1.5)}
+    return render(request, 'bmi/bmi_cal.html' , context)
