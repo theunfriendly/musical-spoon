e9a6da9b859bb6c1aed725a216df09325567c001
ghosthapper <shubhankansu11@gmail.com>
2020-10-22 00:22:41 +0530

    form is created for bmi

--- bmi/urls.py
+++ bmi/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('index/',index,name='index')
+    path('index/',index,name='index'),
+    path('success/<str:id>',success),
 ]
--- bmi/views.py
+++ bmi/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 def calculateBMI(weight, height):
     return weight / (height ** 2)
 
 def index(request):
-    context = {'bmi': calculateBMI(65,1.5)}
-    return render(request, 'bmi/bmi_cal.html' , context)
+    if request.method == "POST":
+        height = request.POST.get('height')
+        weight = request.POST.get('weight')
+        context = {'bmi': calculateBMI(int(height), int(weight))}
+        for i,j in context.items():
+            bmi_cal = j
+        print(bmi_cal,'kkk')
+        return redirect('/bmi/success/'+str(bmi_cal))
+    return render(request, 'bmi/bmi_cal.html',locals())
+
+def success(request,id):
+    print(id)
+    return render(request,'bmi/bmi_success.html',locals())
+
